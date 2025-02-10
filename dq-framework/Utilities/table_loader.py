9016f4f66d624664c620047e8b4a25228d6013e4
Apurva Ananda Lokare(UST <265540@ust.com>
2025-02-10 16:52:03 +0530

    demo_msg

--- dq-framework/Utilities/table_loader.py
+++ dq-framework/Utilities/table_loader.py
@@ -3,6 +3,8 @@ def config_loader(s3_table_path,entity_id):
         """
         load the dataframe from path and filter data for given entity id
 
+        load the dataframe from path and filter data for given entity id
+
         args:
         s3_table_path/table_name : file_path or name of the configuration table.
         entity_id : id for which the data will be fileterd
